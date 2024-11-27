994cabf99d26ce5367dbe8dd6c79a5c2b8eef5e9
ajramirezdev <ajlimramirez@gmail.com>
2024-11-27 10:37:00 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module Graphql
     # Don't generate system test files.
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:5173" # Replace with your frontend's origin
+        origins "*" # Replace with your frontend's origin
 
         resource "*",
                  headers: :any,
